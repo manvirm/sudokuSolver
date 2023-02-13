dfb5b91705ddbeb8de549a4f2d07e9c67897f7ac
Manvir Mann <manvirm29@gmail.com>
2023-02-12 23:19:14 -0800

    Solve grid and print solution

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def is_valid_move(grid, row, col, number):
 
 def solve(grid, row, col):
     # Check if we reached end (sudoku solved)
-    if col = 9:
+    if col == 9:
         if row == 8:
             return True
         row += 1
@@ -40,3 +40,28 @@ def solve(grid, row, col):
 
             if solve(grid, row, col + 1):
                 return True
+        # If not valid move, leave at 0
+        grid[row][col] = 0
+    
+    # No possible solution
+    return False
+
+grid = [[0, 0, 0, 0, 0, 0, 6, 8, 0],
+        [0, 0, 0, 0, 7, 3, 0, 0, 9],
+        [3, 0, 9, 0, 0, 0, 0, 4, 5],
+        [4, 9, 0, 0, 0, 0, 0, 0, 0],
+        [8, 0, 3, 0, 5, 0, 9, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 3, 6],
+        [9, 6, 0, 0, 0, 0, 3, 0, 8],
+        [7, 0, 0, 6, 8, 0, 0, 0, 0],
+        [0, 2, 8, 0, 0, 0, 0, 0, 0]]
+
+# If solution found, print solved grid
+if solve(grid, 0, 0):
+    for i in range(9):
+        for j in range(9):
+            print(grid[i][j], end=" ")
+
+else:
+    print("No solution for this grid")
+        
